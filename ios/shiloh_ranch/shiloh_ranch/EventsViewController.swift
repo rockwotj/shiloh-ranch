c90b10531750e632660c9c86ca6c3b5ad7177a27
rockwotj <rockwotj@gmail.com>
2015-06-08 21:29:45 -0700

    Added Pods and the JTCalendarView for events

--- ios/shiloh_ranch/shiloh_ranch/EventsViewController.swift
+++ ios/shiloh_ranch/shiloh_ranch/EventsViewController.swift
@@ -8,10 +8,36 @@
 
 import UIKit
 
-class EventsViewController: UIViewController {
+class EventsViewController: UIViewController, JTCalendarDataSource {
 
+    @IBOutlet weak var calendarMenuView: JTCalendarMenuView!
+    
+    @IBOutlet weak var calendarContentView: JTCalendarContentView!
+    
+    var calendar : JTCalendar?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendar = JTCalendar()
+        calendar?.calendarAppearance.isWeekMode = true;
+        calendar?.menuMonthsView = calendarMenuView
+        calendar?.contentView = calendarContentView
+        calendar?.dataSource = self
+        calendar?.currentDate = NSDate()
+        calendar?.currentDateSelected = NSDate()
+        calendar?.reloadData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.calendar?.repositionViews()
+    }
+    
+    func calendarDidDateSelected(calendar: JTCalendar!, date: NSDate!) {
+        println("\(date)")
+    }
+    
+    func calendarHaveEvent(calendar: JTCalendar!, date: NSDate!) -> Bool {
+        return false
     }
 
 
